7457dbdafe52672983c188508bb5894074d328ee
Hafez Abdullah <hafez2217-402-137@bsdi-bd.org>
2026-02-13 18:22:47 +0600

    v3

--- src/middleware.ts
+++ src/middleware.ts
@@ -50,5 +50,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)","/api/auth/:path*"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
+  ],
 }
